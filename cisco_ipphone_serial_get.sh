fa6ff1afc1694d5f5bc95a39ea9889750dd4df49
Peter Musolino <pmusolino@rms.com>
2018-06-12 11:40:54 -0400

    Added device os and type

--- cisco_ipphone_serial_get.sh
+++ cisco_ipphone_serial_get.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 #  The following script queries call manager via SNMP to get basic phone data then visits the phones web page to grab serial number.
-# html-xml-utils is required for hxselect 
+# html-xml-utils is required for hxselect
 #GET INDEXES
 phone_index_array=()
 CUCM="1.2.3.4"
-COMMUMNITY="somecommunity"
-CSV_FILE="phone_data.csv"
+COMMUNITY="somecommunity"
 
+#Create file
+CSV_FILE="phone_data.csv"
 /bin/touch $CSV_FILE
+
 # Creating but not really using associative array as it is for debugging.
 #declare -A phone
 # Get Index of all phones
@@ -18,18 +20,26 @@ while read -r line; do
     phone_index_array+=("$line")
 done <<< "$phone_index_string"
 
+# Insert Header
+echo "IP,Serial,Type,Version,User,Description,Registered,Extension" >> $CSV_FILE
+
+# Grab data from each OID based on index and use curl to grab Serial and device type
 for i in ${phone_index_array[@]}; do
     IP=$(/usr/bin/snmpget -Oqv -v2c -c $COMMUNITY $CUCM 1.3.6.1.4.1.9.9.156.1.2.1.1.6.$i)
     USER=$(/usr/bin/snmpget -Oqv -v2c -c $COMMUNITY $CUCM 1.3.6.1.4.1.9.9.156.1.2.1.1.5.$i)
     DESC=$(/usr/bin/snmpget -Oqv -v2c -c $COMMUNITY $CUCM 1.3.6.1.4.1.9.9.156.1.2.1.1.4.$i)
     REGISTERED=$(/usr/bin/snmpget -Oqv -v2c -c $COMMUNITY $CUCM 1.3.6.1.4.1.9.9.156.1.2.1.1.7.$i)
-    DEVICE_TYPE=$(/usr/bin/snmpget -Oqv -v2c -c $COMMUNITY $CUCM 
+    DEVICE_VERSION=$(/usr/bin/snmpget -Oqv -v2c -c $COMMUNITY $CUCM 1.3.6.1.4.1.9.9.156.1.2.1.1.25.$i)
     EXTENSION=$(/usr/bin/snmpwalk -Oqv -v2c -c $COMMUNITY $CUCM 1.3.6.1.4.1.9.9.156.1.2.5.1.2.$i | tr -d '\n')
+    DEVICE_TYPE=$(curl -s 0 $IP/Device_Information.html | awk '/Model\ Number/,/Codec/'  | grep -ie '</tr>' | sed 's/\<\/tr\>//' | hxselect -c b 2>/dev/null)
+    if [ -z "$DEVICE_TYPE" ]; then
+        DEVICE_TYPE=$(curl -s 0 $IP/DeviceInformation | awk '/Model\ Number/,/Codec/' | grep -e '^<TD>' | hxselect -c B 2>/dev/null)
+    fi
     SERIAL=$(curl -s 0 $IP/Device_Information.html | awk '/Serial\ Number/,/Model\ Number/' | grep -ie '</tr>' | sed 's/\<\/tr\>//' | hxselect -c b 2>/dev/null)
     if [ -z "$SERIAL" ]; then
         SERIAL=$(curl -s 0 $IP/DeviceInformation | awk '/Serial\ Number/,/Model\ Number/'  | grep -e '^<TD>' | hxselect -c B 2>/dev/null)
     fi
-    csv="$SERIAL,$USER,$DESC,$REGISTERED,$EXTENSION"
+    csv="$SERIAL,$DEVICE_TYPE,$DEVICE_VERSION,$USER,$DESC,$REGISTERED,$EXTENSION"
 #    phone[$IP]=$csv
     echo "$IP,$csv" >> $CSV_FILE
     done
@@ -39,8 +49,10 @@ for i in ${phone_index_array[@]}; do
 #    echo "IP : $i"
 #    IFS=',' read -r -a phone_value_array <<< "${phone[$i]}"
 #    echo "Serial: ${phone_value_array[0]}"
-#    echo "User: ${phone_value_array[1]}"
-#    echo "Desc: ${phone_value_array[2]}"
-#    echo "Status: ${phone_value_array[3]}"
-#    echo "Extension: ${phone_value_array[4]}"
+#    echo "Device Type : ${phone_value_array[1]}"
+#    echo "Device Version : ${phone_value_array[2]}"
+#    echo "User: ${phone_value_array[3]}"
+#    echo "Desc: ${phone_value_array[4]}"
+#    echo "Status: ${phone_value_array[5]}"
+#    echo "Extension: ${phone_value_array[6]}"
 #    done
